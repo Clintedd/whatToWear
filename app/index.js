90f79b978101edfcb2decdcfd55a7238a4e53376
Clint Lee <l.clint91@gmail.com>
2018-05-07 09:22:56 -0400

    gh-pages stuff

--- app/index.js
+++ app/index.js
@@ -12,18 +12,18 @@ const hairs = {
 
 const eyes = {
     prefix: "you will",
-    bareEyes: ["meet", "punch"],
-    glasses: ["kiss", "whip"],
+    bareEyes: ["meet", "make fun of"],
+    glasses: ["break dance with", "play peekaboo with"],
     sunGlasses: ["greet", "fight"],
     contactLenses: ["bite", "sue"]
 }
 
 const mouths = {
     sufix: "in",
-    bareMouth: ["Donald Trump", "Robert Downey Jr", "Danny Pack", "Heather Payne"],
+    bareMouth: ["Donald Trump", "Robert Downey Jr", "Danny Pack", "Nicky Minaj"],
     lipstick: ["Ryan Christiani", "Ryan Gosling", "DJ.Khalid(cuz another one)"],
     beard: ["Kim Jong Un", "Justin Bieber", "Drake"],
-    mustache: ["weio"]
+    mustache: ["Keira Knightley", "John Travolta"]
 }
 
 const upperBodys = {
@@ -31,25 +31,25 @@ const upperBodys = {
     tShirt: ["Canada", "North Korea"],
     longsleevs: ["USA", "Russia"],
     tankTop: ["Brazil", "Croatia"],
-    dressShirt: ["France"],
-    blouse: ["South Korea"]
+    dressShirt: ["France", "Mexico"],
+    blouse: ["South Korea", "Chile"]
 }
 
 const lowerBodys = {
-    bareLower: ["sd"],
+    bareLower: ["because you were"],
     shorts: ["after"],
     jeans: ["for"],
     skirt: ["while"],
-    dressPants: ["qw"],
-    yogaPants: ["asd"]
+    dressPants: ["for"],
+    yogaPants: ["after"]
 }
 
 const footwears = {
-    bareFoot: ["trying to fry an egg with someone else's body heat"],
-    snickers: ["tweeting Danny's secret"],
-    converse: ["breaking someone's neck highfiving"],
-    dressShoes: ["hurling chimpanzee's feces that were thrown at you"],
-    highHeels: ["wi"]
+    bareFoot: ["trying to fry an egg with someone else's body heat", "firing someone just because that person had a bad B/O", "finding nemo", "congratulating "],
+    snickers: ["tweeting Danny's secret", "fighting a bearcub for your last piece of them precious popeyes chicken", "listening to drake try to sing 'My heart will go on' on the show, 'Dancing with the Stars' that nobody watches"],
+    converse: ["breaking someone's neck highfiving", "telling a kid Santa never bothered to visit his house because his mom's cookies were bad", "butchering a song with your voice"],
+    dressShoes: ["hurling chimpanzee's feces that were thrown at you", "acting like a sassy old lady", "picking your nose"],
+    highHeels: ["eating the last piece of chocolate that were given to orphans", "eating popeyes allday erday", "showing South Park to your 8 year old son thinking its a kid's show"]
 }
 
 const randomItemInArray = function (array) {
